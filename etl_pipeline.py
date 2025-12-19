f2abb9ef587e2796bc27ff176e2bebe04ad92b4c
Veerapat Pithaksinakorn <pithaksinakorn@gmail.com>
2025-12-19 12:03:45 +0700

    Fix ip

--- etl_pipeline.py
+++ etl_pipeline.py
@@ -139,7 +139,7 @@ def deploy_to_database(fact_table, dim_tables):
     print("\n🚀 Deploying to Database...")
     
     # Database configuration
-    server = "34.55.36.66"
+    server = "34.63.152.130"
     database = 'TestDB'
     username = 'SA'
     password = os.getenv('DB_PASSWORD', 'Passw0rd123456')
